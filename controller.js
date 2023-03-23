daa73865e4c70672c2ca820133fd90706c4af5de
Mohit Punia <97046203+MOHIT-0001@users.noreply.github.com>
2023-03-23 18:37:56 +0530

    updated

--- controller.js
+++ controller.js
@@ -1,52 +1,98 @@
-    let seconds = 00; 
-    let tens = 00; 
-    let appendTens = document.getElementById("tens")
-    let appendSeconds = document.getElementById("seconds")
-    let buttonStart = document.getElementById('button-start');
-    let buttonStop = document.getElementById('button-stop');
-    let buttonReset = document.getElementById('button-reset');
-    let Interval ;
-  
+let hours = 00;
+let minutes = 00;
+let seconds = 00;
+let tens = 00;
+let appendTens = document.getElementById("tens")
+let appendSeconds = document.getElementById("seconds")
+let appendMinutes = document.getElementById("minutes")
+let appendHours = document.getElementById("hours")
+let buttonStart = document.getElementById('button-start');
+let buttonStop = document.getElementById('button-stop');
+let buttonReset = document.getElementById('button-reset');
+let buttonLap = document.getElementById('button-lap');
+let lapTimeList = document.getElementById('lap-time-list');
+let Interval;
+
+buttonStart.onclick = function () {
+  buttonStart.style.display = 'none'
+  buttonStop.style.display = 'inline-block'
+  buttonLap.style.display = 'inline-block'
+  lapTimeList.style.display = 'block'
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+}
+
+buttonStop.onclick = function () {
+  clearInterval(Interval);
+  buttonStop.style.display = 'none'
+  buttonStart.style.display = 'inline-block'
+  buttonLap.style.display = 'none'
+}
+
+buttonReset.onclick = function () {
+  // lapTimeList.style.display='none'
+  buttonStop.style.display = 'none'
+  buttonLap.style.display = 'none'
+  buttonStart.style.display = 'inline-block'
+  lapTimeList.innerHTML = ''
+
+  clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  minutes='00';
+  hours='00';
+  appendTens.innerHTML = tens;
+  appendSeconds.innerHTML = seconds;
+  appendMinutes.innerHTML = minutes;
+  appendHours.innerHTML = hours;
+}
+
+buttonLap.onclick = function () {
+  var presentTime = hours + ':' + minutes + ':' + seconds + ':' + tens
+  const li = document.createElement("li");
+  const textNode = document.createTextNode(presentTime)
+  li.append(textNode);
+  lapTimeList.insertBefore(li, lapTimeList.children[0])
+}
 
 
+function startTimer() {
+  tens++;
+  if (tens <= 9) {
+    appendTens.innerHTML = "0" + tens;
+  }
 
-    buttonStart.onclick = function() {
-      clearInterval(Interval);
-       Interval = setInterval(startTimer, 10);
-    }
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
 
-      buttonStop.onclick = function() {
-         clearInterval(Interval);
-    }
+  if (tens > 99) {
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
 
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+  if (seconds > 59) {
+    minutes++;
+    appendMinutes.innerHTML = '0' + minutes;
+    seconds = 0;
+    appendSeconds.innerHTML = "0" + 0;
+  }
+  if (minutes > 9) {
+    appendMinutes.innerHTML = minutes;
+  }
+  if (minutes > 59) {
+    hours++;
+    appendHours.innerHTML = '0' + hours;
+    minutes = 0;
+    appendMinutes.innerHTML = "0" + 0;
+  }
+  if (hours > 9) {
+    appendHours.innerHTML = hours
+  }
 
-    buttonReset.onclick = function() {
-            clearInterval(Interval);
-      tens = "00";
-        seconds = "00";
-      appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-    }
-    
-    
-    function startTimer () {
-      tens++; 
-      if(tens <= 9){
-        appendTens.innerHTML = "0" + tens;
-      }
-      
-      if (tens > 9){
-        appendTens.innerHTML = tens;
-        } 
-      
-      if (tens > 99) {
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        appendTens.innerHTML = "0" + 0;
-      }
-      
-      if (seconds > 9){
-        appendSeconds.innerHTML = seconds;
-      }
-    }
+}
